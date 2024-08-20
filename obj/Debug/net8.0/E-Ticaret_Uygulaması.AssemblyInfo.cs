d4b66151f090f51043074b9b42190151004a4546
Ali Kaan Karagözgil <kaan.karagozgil@icloud.com>
2024-08-20 12:05:25 +0300

    çalıaşıyor acndslknflds

--- obj/Debug/net8.0/E-Ticaret_Uygulaması.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-Ticaret_Uygulaması.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Ticaret_Uygulaması")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52dc73dc4009c5ff1d4da22bd652dee5347fc034")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Ticaret_Uygulaması")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Ticaret_Uygulaması")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
