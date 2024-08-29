8de63f7e87cd16412892bcff9e73f864496f5901
Adelenarci <kaan.karagozgil@icloud.com>
2024-08-29 08:45:06 +0300

    d

--- obj/Debug/net8.0/E-Ticaret_Uygulaması.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-Ticaret_Uygulaması.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Ticaret_Uygulaması")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a565be391b9a037ad5babfa0a5f81580766d3211")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3723ce53ffb41fa8033bb330fd4a9c75e4488f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Ticaret_Uygulaması")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Ticaret_Uygulaması")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
